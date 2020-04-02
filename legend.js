a413c9cdc54dc4eece31604671869a8f7ab9d65f
sproutseeds <codyshanemitchell@gmail.com>
2020-04-02 16:40:45 -0700

    Added a Legend.

---
+++ legend.js
@@ -0,0 +1,28 @@
+let ul = document.getElementById("legend_list");
+
+// const color_array = [
+//   "#FFE5E5",
+//   "#FFCCCC",
+//   "#FFB2B2",
+//   "#FF7F7F",
+//   "#FF6565",
+//   "#FF4C4C",
+//   "#FF3232"
+// ];
+
+const color_description = [
+  "0.00% > 0.01%",
+  "0.01% > 0.03%",
+  "0.03% > 0.05%",
+  "0.05% > 0.08%",
+  "0.08% > 0.11%",
+  "0.11% > 0.2%",
+  "0.2%"
+];
+
+for (color in color_description) {
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(`${color_description[color]}`));
+  li.setAttribute("id", `color_${parseInt(color)}`);
+  ul.appendChild(li);
+}
